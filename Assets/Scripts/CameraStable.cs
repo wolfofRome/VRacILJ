3adb70c04e58480329be5677d07efa9bd82ed529
wolfofRome <prideofguardian0607@gmail.com>
2019-11-16 11:36:19 +0100

    :sparkles: New car controller
    Using basic asset from unity, car model to be changed

---
+++ Assets/Scripts/CameraStable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour{
+
+    public GameObject followedCar;
+    public float carX;
+    public float carY;
+    public float carZ;
+
+    void Update(){
+        carX = followedCar.transform.eulerAngles.x;
+        carY = followedCar.transform.eulerAngles.y;
+        carZ = followedCar.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(carX - carX, carY, carZ - carZ);
+    }
+}
