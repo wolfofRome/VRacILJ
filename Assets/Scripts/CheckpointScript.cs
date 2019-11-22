056a105e22f04c90e0a997aea7c49affcb0d313c
wolfofRome <prideofguardian0607@gmail.com>
2019-11-22 15:01:28 +0100

    :sparkles: Added simple lap system with checkpoints

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(!(other.CompareTag("Car") || other.CompareTag("Player")))
+            return;
+
+        CarCheckpointScript ccs = other.transform.root.gameObject.GetComponent<CarCheckpointScript>();
+        if(ccs.IsNextCheckpoint(transform))
+        {
+            Debug.Log("Checkpoint validé : " + gameObject.name);
+            ccs.NextCheckpoint();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LapSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapSystem : MonoBehaviour
+{
+    private static LapSystem instance;
+    public static LapSystem Instance
+    {
+        get { return instance; }
+    }
+    public int maxLaps = 3;
+    public List<Transform> checkpointsList;
+
+    private void Awake() {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        checkpointsList = new List<Transform>();
+        foreach(CheckpointScript checkpoint in GetComponentsInChildren<CheckpointScript>())
+            checkpointsList.Add(checkpoint.transform);
+        Debug.Log(checkpointsList.Count);
+    }
+
+    public Transform FirstCheckpoint()
+    {
+        return checkpointsList[0];
+    }
+
+    public Transform LastCheckpoint()
+    {
+        return checkpointsList[checkpointsList.Count-1];
+    }
+
+    public Transform NextCheckpoint(ref int indexCurrentCheckpoint)
+    {
+        indexCurrentCheckpoint = (indexCurrentCheckpoint + 1) % checkpointsList.Count;
+        return checkpointsList[indexCurrentCheckpoint];
+    }
+}
