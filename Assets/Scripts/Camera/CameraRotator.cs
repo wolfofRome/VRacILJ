261de1729da3a5abd6cfa70272c924e12750d1bf
wolfofRome <prideofguardian0607@gmail.com>
2020-01-04 01:24:06 +0100

    :sparkles: Menu that brings to custom race track scene

---
+++ Assets/Scripts/Camera/CameraRotator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+	private Vector3 localRotation;
+	private Transform parentTransform;
+    
+    public float VerticalSpeed;
+    public float HorizontalSpeed;
+	
+	[Range(0.01f, 1.0f)]
+    public float SmoothFactor;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		parentTransform = transform.parent;
+		localRotation = new Vector3(-100, 20, 0);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+		localRotation.x += Input.GetAxis("Mouse X") * HorizontalSpeed;
+		localRotation.y += Input.GetAxis("Mouse Y") * VerticalSpeed;
+
+		localRotation.y = Mathf.Clamp(localRotation.y, 0f, 90f);
+
+		Quaternion quat = Quaternion.Euler(localRotation.y, localRotation.x, 0);
+		parentTransform.rotation = Quaternion.Lerp(parentTransform.rotation, quat, SmoothFactor);
+	}
+}
