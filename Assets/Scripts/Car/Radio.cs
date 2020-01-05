67a864309ee40e7aec0f084ecb341ef07b21aa9f
wolfofRome <prideofguardian0607@gmail.com>
2020-01-05 22:00:14 +0100

    :construction: radio

---
+++ Assets/Scripts/Car/Radio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private List<AudioClip> m_Tracks;
+    void Awake()
+    {
+        m_Tracks = new List<AudioClip>();
+        string dir = (Directory.GetCurrentDirectory()+"\\Assets\\Audio\\radio");
+        string[] paths = Directory.GetFiles(dir);
+        foreach(string path in paths) {
+            Debug.Log(path);
+            m_Tracks.Add((AudioClip)Resources.Load(path));
+        }
+        audioSource.PlayOneShot(m_Tracks[0]);
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
