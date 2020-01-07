c70444217f94df5c08120f3b02e28bbc76382ab5
wolfofRome <prideofguardian0607@gmail.com>
2020-01-07 17:12:02 +0100

    :sparkles: Auto recenter of camera when not moving

--- Assets/Scripts/Camera/CameraUserControlled.cs
+++ Assets/Scripts/Camera/CameraUserControlled.cs
@@ -7,59 +7,53 @@ public class CameraUserControlled : MonoBehaviour
     public float m_maxX = 15, m_minX = -5, m_maxY = 80, m_minY = -80;
     public GameObject parent;
 
-    //How much the camera is rotated compared to its start rotation
-    private Vector3 rotationAdjust = new Vector3(0,0,0);
+    public float cameraReadjustSpeed = 2;
+    public float delayBeforeReadjust = .25f;
 
-    private int m_CameraSmoothing = 1;
-
-
+    private float m_ReadjustTimer = 0;
 
     void Update()
     {
         float xAxis = Input.GetAxis("Mouse X");
         float yAxis = Input.GetAxis("Mouse Y");
         
-        if(false && xAxis == 0 && yAxis == 0)
+        if(xAxis == 0 && yAxis == 0)
         {
-            /*
-            float readjustSpeed = 5;
-            Vector3 currentRotation = transform.eulerAngles;
-            Vector3 adjustRotation = new Vector3(0,0,0);
-            
-            if (currentRotation.x < 180)
+            m_ReadjustTimer += Time.deltaTime;
+            if(m_ReadjustTimer > delayBeforeReadjust)
             {
-                adjustRotation.x = currentRotation.x;
-                adjustRotation.x = Mathf.Clamp(adjustRotation.x, 0, readjustSpeed);
-                adjustRotation.x *= -1;
-            }
-            else
-            {
-                adjustRotation.x = 360 - currentRotation.x;
-                adjustRotation.x = Mathf.Clamp(adjustRotation.x, 0, readjustSpeed);
-            }
-            if (currentRotation.y < 180)
-            {
-                adjustRotation.y = currentRotation.y;
-                adjustRotation.y = Mathf.Clamp(adjustRotation.y, 0, readjustSpeed);
-                adjustRotation.y *= -1;
-            }
-            else
-            {
-                adjustRotation.y = 360 - currentRotation.y;
-                adjustRotation.y = Mathf.Clamp(adjustRotation.y, 0, readjustSpeed);
-            }
+                Vector3 currentRotation = transform.localEulerAngles;
+                Vector3 adjustRotation = new Vector3(0, 0, 0);
 
-            Debug.Log("Current rotation " + currentRotation + "\nDelta : " + adjustRotation);
-
-
-            transform.Rotate(adjustRotation, Space.Self);
-
-            rotationAdjust += adjustRotation;
-            //transform.Rotate(Vector3.Lerp(transform.localEulerAngles, adjustRotation, Time.deltaTime * 1),Space.Self);
-            */
+                if (currentRotation.x < 180)
+                {
+                    adjustRotation.x = currentRotation.x;
+                    adjustRotation.x = Mathf.Clamp(adjustRotation.x, 0, cameraReadjustSpeed);
+                    adjustRotation.x *= -1;
+                }
+                else
+                {
+                    adjustRotation.x = 360 - currentRotation.x;
+                    adjustRotation.x = Mathf.Clamp(adjustRotation.x, 0, cameraReadjustSpeed);
+                }
+                if (currentRotation.y < 180)
+                {
+                    adjustRotation.y = currentRotation.y;
+                    adjustRotation.y = Mathf.Clamp(adjustRotation.y, 0, cameraReadjustSpeed);
+                    adjustRotation.y *= -1;
+                }
+                else
+                {
+                    adjustRotation.y = 360 - currentRotation.y;
+                    adjustRotation.y = Mathf.Clamp(adjustRotation.y, 0, cameraReadjustSpeed);
+                }
+                //Debug.Log("Current rotation " + currentRotation + "\nDelta : " + adjustRotation);
+                transform.Rotate(adjustRotation, Space.Self);
+            }            
         }
         else
         {
+            m_ReadjustTimer = 0.0f;
             Debug.Log("Current rotation : " + transform.localEulerAngles);
             yAxis *= -1;
             transform.Rotate(new Vector3(yAxis,xAxis,0.0f),Space.World);
@@ -67,14 +61,16 @@ public class CameraUserControlled : MonoBehaviour
             if ((transform.localEulerAngles.y > m_maxY && transform.localEulerAngles.y < 180) ||
                 (transform.localEulerAngles.y < 360 + m_minY && transform.localEulerAngles.y > 180))
             {
-                transform.Rotate(new Vector3(0, -xAxis, 0.0f));
+                transform.Rotate(new Vector3(0.0f, -xAxis, 0.0f));
             }
 
             if ((transform.localEulerAngles.x > m_maxX && transform.localEulerAngles.x < 180) ||
                 (transform.localEulerAngles.x < 360 + m_minX && transform.localEulerAngles.x > 180))
             {
-                transform.Rotate(new Vector3(-yAxis, 0, 0.0f));
+                transform.Rotate(new Vector3(-yAxis, 0.0f, 0.0f));
             }
         }
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
     }
 }
